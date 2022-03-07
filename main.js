e8ab2d6ae455095daa79d586d16d0b4c6a135cf3
Christian Podda <poddachristian94@gmail.com>
2022-03-07 19:52:25 +0100

    Selected but now working

---
+++ main.js
@@ -0,0 +1,15 @@
+// Console Check
+console.log("JS OK!");
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+hamburgerMenu.addEventListener('click', function () {
+    alert("Hello World!");
+});
+
+
+// document.addEventListener("click", function () {
+//     alert("Hello World!");
+// });
+
+
+// document.getElementById(id).onclick = function(){code}
